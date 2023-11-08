f7892d027b5f59683dd0f0e9806da19b573055c2
YooJeong <91018849+syj02719@users.noreply.github.com>
2023-11-08 14:16:00 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    
+    int A, B;
+
+    while (std::cin >> A >> B ) {
+        std::cout << A + B << "\n"; 
+
+    }
+
+    return 0; 
+
+}
