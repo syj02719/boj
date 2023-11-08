34d381d380a11b25e46f057057bb72c19763ca1c
YooJeong <91018849+syj02719@users.noreply.github.com>
2023-11-08 14:08:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    long long num1, num2, num3;
+    scanf("%lld %lld %lld", &num1, &num2, &num3);
+
+    printf("%lld", num1 + num2 + num3);
+
+    return 0;
+}
