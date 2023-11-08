e362a5690449e390a0855bace4aa6982f9ae3dbf
YooJeong <91018849+syj02719@users.noreply.github.com>
2023-11-08 14:15:08 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    
+    int T, A, B;
+    std::cin >> T;
+
+    for (int i = 1; i < T+1; i++) {
+        std::cin >> A >> B;
+        std::cout << "Case #" << i << ": " << A + B << "\n";
+    }
+
+    return 0; 
+}
